556fa171e72c67c60e19842d00876ecd45f96389
hejie <565703319@qq.com>
2020-11-10 23:24:21 +0800

    [hj] finish practice2

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,10 @@
-const inject
+const inject = (items, sections) => {
+  let flag = 0;
+  for (const section of sections) {
+    const { content, index } = section;
+    items.splice(index + flag, 0, content);
+    flag++;
+  }
+  return items;
+};
 export { inject };
