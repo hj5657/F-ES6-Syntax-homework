081d4c08041cf4fc389d04d44e4888cc0a94f4ee
hejie <565703319@qq.com>
2020-11-10 22:04:09 +0800

    [hj] finish practice1

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
+const parseData = (input) => {
+  const { data, column } = input;
+  let output = [];
+  for (let i = 0; i < data.length; i++) {
+    let dataInfo = data[i];
+    let outputValue = {};
+    for (let j = 0; j < column.length; j++) {
+      outputValue[column[j].name] = dataInfo[j];
+    }
+    output.push(outputValue);
+  }
+  return output;
+};
 export { parseData };
