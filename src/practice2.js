b6bbc1b7ba4390633a2ce6fbf1574970fee31c67
hejie <565703319@qq.com>
2020-11-12 19:07:16 +0800

    [hj] finish practice2 use sort

--- src/practice2.js
+++ src/practice2.js
@@ -1,9 +1,19 @@
+const compare = (object1, object2) => {
+  const x = object1.index;
+  const y = object2.index;
+  if (x > y) {
+    return -1;
+  } else if (x < y) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
 const inject = (items, sections) => {
-  let flag = 0;
+  sections.sort(compare);
   for (const section of sections) {
     const { content, index } = section;
-    items.splice(index + flag, 0, content);
-    flag++;
+    items.splice(index, 0, content);
   }
   return items;
 };
